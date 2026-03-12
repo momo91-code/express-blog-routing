ab35018a0404fec0acc702a74b036e74b8788fba
monica <monica.poli91@libero.it>
2026-03-12 18:16:07 +0100

    implementazione e completamento crud

--- controllers/postController.js
+++ controllers/postController.js
@@ -40,9 +40,68 @@ function destroy(req, res) {
     console.log(`post ${id} eliminato`, posts)
     return res.sendStatus(204)
 }
+
+function store(req, res) {
+
+    const newPost = {
+        id: posts[posts.length - 1].id + 1,
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        tags: req.body.tags,
+    }
+    posts.push(newPost)
+    return res.status(201).json(newPost)
+};
+
+function update(req, res) {
+    const id = Number(req.params.id)
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "user error", message: "id non valido" })
+
+    }
+    const result = posts.find(post => post.id == id)
+    if (!result) {
+        return res.status(404).json({ error: "not found", message: " post non trovato" })
+    }
+
+    result.title = req.body.title ?? "";
+    result.content = req.body.content ?? "";
+    result.image = req.body.image ?? "";
+    result.tags = req.body.tags ?? [];
+
+    return res.json(result);
+};
+
+function modify(req, res) {
+    const id = Number(req.params.id)
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "user error", message: "id non valido" })
+
+    }
+    const result = posts.find(post => post.id == id)
+    if (!result) {
+        return res.status(404).json({ error: "not found", message: " post non trovato" })
+    }
+
+
+    const allowedProperties = ["title", "content", "image", "tags"]
+    for (const propertyName of allowedProperties) {
+        if (req.body[propertyName] !== undefined) {
+            result[propertyName] = req.body[propertyName]
+        }
+    }
+    return res.json(result)
+
+};
+
+
 const container = {
     index,
     show,
-    destroy
+    destroy,
+    store,
+    update,
+    modify
 }
 module.exports = container;
--- routers/posts.js
+++ routers/posts.js
@@ -3,56 +3,11 @@ const router = express.Router();
 const postsController = require('../controllers/postController')
 
 router.get('/', postsController.index)
-
 router.get('/:id', postsController.show)
-
-
-router.post('/', (req, res) => {
-    res.send(`You are creating a new post`)
-});
-
-// questo codice mostra il nuovo post che hai deciso di creare
-/*router.post('/', (req, res) => {
-    const newPost = req.body
-    posts.push(newPost)
-    res.status(201).json(newPost)
-})*/
-
-router.put('/:id', (req, res) => {
-    res.send(`You requested to update the post with id: ${req.params.id}`)
-});
-
-// questo codice mostra update post con determinato id
-/*router.put('/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const index = posts.findIndex(p => p.id === id)
-    if (index !== -1) {
-        posts[index] = { ...req.body, id }  // mantiene l'id originale
-        res.json(posts[index])
-    } else {
-        res.status(404).send("Post non trovato")
-    }
-})*/
-
-router.patch('/:id', (req, res) => {
-    res.send(`You requested to modify the post with id: ${req.params.id}`)
-});
-
-// questo codice mostra modifica post con determinato id
-/*router.patch('/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const post = posts.find(p => p.id === id)
-    if (post) {
-        Object.assign(post, req.body)  // aggiorna solo i campi presenti nel body
-        res.json(post)
-    } else {
-        res.status(404).send("Post non trovato")
-    }
-})*/
-
-
-
 router.delete('/:id', postsController.destroy)
+router.post('/', postsController.store)
+router.put('/:id', postsController.update)
+router.patch('/:id', postsController.modify)
 
 
 
